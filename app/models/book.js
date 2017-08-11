93908ffb2fa655eea2f1329885958dd366b5ce84
Dhruv <pythonbrilliant@gmail.com>
2017-08-12 00:04:32 +0530

    Changed Library

---
+++ app/models/book.js
@@ -0,0 +1,16 @@
+// load the things we need
+var mongoose = require('mongoose');
+// define the schema for our user model
+var eventScheme = mongoose.Schema({
+
+    name : String ,
+    author : String,
+    year : String ,
+    description : String ,
+    image : String ,
+    link : String , 
+    category : String
+});
+
+// create the model for Evetns and expose it to our app
+module.exports = mongoose.model('Book', eventScheme);
