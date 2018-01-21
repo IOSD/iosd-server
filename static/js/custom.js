3cf04eea76253bd6df62c3106c08c998258e5cca
Dhruv Ramdev <pythonbrilliant@gmail.com>
2018-01-22 00:20:19 +0530

    Changes in Mentor and Event on Dashbaord

---
+++ static/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#events-slides').owlCarousel({
+        loop:true,
+        items:1,
+        center:true,
+        autoplay : true
+    })
+
+    $('#mentor-slider').owlCarousel({
+        loop:true,
+        items:1,
+        center:true,
+        autoplay : true
+    })
+});
